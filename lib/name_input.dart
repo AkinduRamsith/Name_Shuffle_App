1edf0fb33a135cac3a8e11cd068ae46b1828e15d
AkinduRamsith <akiyaramsith2002@gmail.com>
2023-11-27 11:19:41 +0530

    add some validations

--- lib/name_input.dart
+++ lib/name_input.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:name_shuffle_app/shuffle.dart';
 
@@ -14,9 +12,7 @@ class _NameState extends State<Name> {
   List<String> studentList = [];
   final TextEditingController _studentController = TextEditingController();
 
-
   void _validateStudent() {
-  
     String studentName = _studentController.text.trim();
     if (studentName.isEmpty || studentList.contains(studentName)) {
       showDialog(
@@ -43,8 +39,41 @@ class _NameState extends State<Name> {
         _studentController.clear();
       });
     }
-  
-}
+  }
+
+  void _clear() {
+    setState(() {
+      studentList.clear();
+    });
+  }
+
+  void _checkList() {
+    if (studentList.length < 5) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Please Enter Minimum 5 Students'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  Center(child: Shuffle(studentList: studentList))));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -78,38 +107,42 @@ class _NameState extends State<Name> {
                 FilledButton(
                     onPressed: () {
                       setState(() {
-                       _validateStudent();
+                        _validateStudent();
                       });
                     },
                     style: FilledButton.styleFrom(
-                      backgroundColor: Colors.deepOrange,
-                        textStyle: const TextStyle(fontSize: 25,fontWeight: FontWeight.bold,fontStyle: FontStyle.italic)),
+                        backgroundColor: Colors.deepOrange,
+                        textStyle: const TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic)),
                     child: const Text("Add Student")),
                 const SizedBox(
                   width: 20,
                 ),
                 FilledButton(
                     onPressed: () {
-                      setState(() {});
+                      _clear();
                     },
                     style: FilledButton.styleFrom(
-                      backgroundColor: Colors.greenAccent,
-                        textStyle: const TextStyle(fontSize: 25,fontWeight: FontWeight.bold,fontStyle: FontStyle.italic)),
+                        backgroundColor: Colors.greenAccent,
+                        textStyle: const TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic)),
                     child: const Text("Clear List")),
               ],
             ),
           ),
           ElevatedButton(
               onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>  Center(child: Shuffle(
-                          studentList:studentList
-                        ))));
+                _checkList();
               },
               style: ElevatedButton.styleFrom(
-                  textStyle: const TextStyle(fontSize: 25,fontWeight: FontWeight.bold,fontStyle: FontStyle.italic)),
+                  textStyle: const TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic)),
               child: const Text("Shuffle")),
           const SizedBox(
             height: 30,
@@ -134,5 +167,3 @@ class _NameState extends State<Name> {
     );
   }
 }
-
-
